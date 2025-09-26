eef9ac6290cfd14a3e3586ed98c14dda941f7bc5
Deathemonic <zinth.chan@tuta.io>
2025-09-26 11:33:25 +0800

    feat: set uniffi optional on rlib

--- build.rs
+++ build.rs
@@ -1,3 +1,4 @@
 fn main() {
+    #[cfg(feature = "uniffi")]
     uniffi::generate_scaffolding("src/baad_core.udl").unwrap();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,16 @@ pub mod formatter;
 
 pub use error::IntoEyreReport;
 
-mod bindings;
 mod utils;
 
 pub use tracing::{debug, error, info, trace, warn};
 pub use utils::{run, run_async};
 
+#[cfg(feature = "uniffi")]
+mod bindings;
+
+#[cfg(feature = "uniffi")]
 pub use bindings::*;
+
+#[cfg(feature = "uniffi")]
 uniffi::include_scaffolding!("baad_core");
